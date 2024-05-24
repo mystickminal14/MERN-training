bba0f7230805b33bea78d67fb76c0e4d0b01e064
Minal <minal.pariyar@patancollege.edu.np>
2024-05-24 08:37:20 +0545

    day -4 loop

---
+++ lesson-4/lesson-4.js
@@ -0,0 +1,48 @@
+// // for(i=1;i<=10;i++){
+// //    if(i===5 ||i===6 ||i===7){
+// //     continue;
+// //    }
+// //    console.log(i)
+   
+// // }
+// let i =2
+// while(i<=10){
+//     if(i %2===0){
+//         console.log(i)
+//     }
+//  i++
+// }
+// //calculate sum of natural numbers
+// let sum=0
+// for(i=1;i<=10;i++){
+//     sum+=i
+
+// }
+// console.log(sum)
+
+//calculate factorial of given number
+let fact=5
+var vv=1
+while(fact>0){
+    vv *= fact
+    fact--
+}
+console.log(vv)
+for(i=1;i<=5;i++){
+    let sq=i**2
+    console.log(sq)
+}
+for(i=1;i<=5;i++){
+    let sq=i**3
+    console.log(sq)
+}
+let names=['minal','kai','vulnerable']
+for(i=0;i<names.length;i++){
+    console.log(names[i])
+}
+let sum=[1,2,3,4,5]
+let value=0
+for(i=0;i<sum.length;i++){
+    value+=sum[i]
+}
+console.log(value)
